ef8557ed7048667518220345270ca67825933b4b
Philip Ni <philipni122@gmail.com>
2015-07-24 11:49:15 -0400

    Added conky and cairo-dock scripts.

---
+++ start_cairo-dock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Starts GLX/Cairo Dock using OpenGL
+cairo-dock -o
+
+# Starts GLX/Cairo Dock using default backend
+#cairo-dock -c
+
+# Starts GLX/Cairo Dock in "management" mode
+#cairo-dock -m
---
+++ start_conky.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Wait a few seconds
+#sleep 5
+
+## Run Conky
+conky
